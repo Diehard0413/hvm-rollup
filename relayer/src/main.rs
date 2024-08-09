44d0738281bdb420701c5b14897aee47377b5c6d
diehard0413 <diehard0413@gmail.com>
2024-08-09 10:17:40 +0200

    updated relayer with offchain labs

--- relayer/src/main.rs
+++ relayer/src/main.rs
@@ -1,9 +1,9 @@
 use clap::Parser;
-use hvm_relayer::{calldata, connect, relay, runtime};
+use hvm_relayer::{commands, calldata, connect, relay, runtime, offchain_lab};
 use log::info;
 
 #[derive(Debug, Parser)]
-#[command(name = "hvm_relayer", about = "offchain relayer", version)]
+#[command(name = "hvm_relayer", about = "HVM offchain relayer", version)]
 struct Cli {
     #[command(subcommand)]
     command: Commands,
@@ -17,36 +17,25 @@ enum Commands {
     FetchCalldata(calldata::FetchOpts),
     #[command(arg_required_else_help = true)]
     Relay(relay::RelayOpts),
+    #[command(arg_required_else_help = true)]
+    OffchainLab(offchain_lab::OffchainLabOpts),
 }
 
 #[tokio::main]
-async fn main() {
-    let opts = relay::RelayOpts::parse();
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    env_logger::init();
 
-    if let Err(e) = relay::run(opts).await {
-        eprintln!("Relayer failed to start: {:?}", e);
-        std::process::exit(1);
+    let cli = Cli::parse();
+
+    match cli.command {
+        Commands::Connect(opts) => connect::start(opts).await?,
+        Commands::FetchCalldata(opts) => {
+            let calldata = calldata::fetch(opts).await?;
+            info!("Fetched {} calldata items", calldata.len());
+        }
+        Commands::Relay(opts) => relay::run(opts).await?,
+        Commands::OffchainLab(opts) => offchain_lab::run(opts).await?,
     }
+
+    Ok(())
 }
-
-// fn main() {
-//     env_logger::init();
-//     let args = Cli::parse();
-
-//     info!("Starting hvm relayer");
-
-//     match args.command {
-//         Commands::Connect(opts) => {
-//             let rt = runtime::get_rt(opts.threads);
-//             rt.block_on(connect::start(opts));
-//         }
-//         Commands::FetchCalldata(opts) => {
-//             let rt = runtime::get_rt(opts.threads);
-//             rt.block_on(calldata::fetch(opts));
-//         }
-//         Commands::Relay(opts) => {
-//             let rt = runtime::get_rt(opts.threads);
-//             rt.block_on(relay::start(opts));
-//         }
-//     }
-// }
